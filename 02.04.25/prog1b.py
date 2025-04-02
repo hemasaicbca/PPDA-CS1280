91cb384ad9217258660abc256655ce28e12e9b62
hemasaicbca <hemasaicbca24@rvu.edu.in>
2025-04-02 15:23:04 +0530

    Add files via upload

---
+++ 02.04.25/prog1b.py
@@ -0,0 +1,16 @@
+def temperature_converter():
+    print("Temperature Converter")
+    temp=float(input("Enter Temperature:"))
+    print("Convert to:")
+    print("1.Celsius to Fahrenheit")
+    print("2.Fahrenheit to celsius")
+    choice=input("Enter choice(1/2):")
+    if choice=='1':
+        fahrenheit=(temp*9/5)+32
+        print(f"{temp}C is {fahrenheit}F")
+    elif choice=='2':
+        celsius=(temp-32)*5/9
+        print(f"{temp}F is {celsius}C")
+    else:
+        print("invalid input")
+temperature_converter()
